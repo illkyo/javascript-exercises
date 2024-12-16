ef51ef4387b5cd861f285c671796f3eab9a5efdb
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2024-12-17 02:56:10 +0500

    feat: complete exercises 1 - 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+  let newArray = [];
+  for (const element of array) {
+    let removeElement = false;
+    for (const elementToRemove of elementsToRemove) {
+      if (element === elementToRemove) {
+        removeElement = true;
+      }
+    }
+    if (!removeElement) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+  if (startNum < 0 || endNum < 0 || !(Number.isInteger(startNum)) || !(Number.isInteger(endNum))) {
+    return 'ERROR'
+  } else {
+    let sum = startNum;
+    let i = startNum;
+  
+    if (startNum > endNum) {
+      while(i > endNum) {
+        sum += --i;
+      }
+    } else {
+      while(i < endNum) {
+        sum += ++i;
+      }
+    }
+  
+    return sum;
+  }
 };
 
 // Do not edit below this line
